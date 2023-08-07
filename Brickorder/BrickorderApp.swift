e3953f104f4c543ea38ab228b6fbe379b282c944
Robert <90681378+robiv2007@users.noreply.github.com>
2023-08-07 11:56:54 +0200

    Initial Commit

---
+++ Brickorder/BrickorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrickorderApp.swift
+//  Brickorder
+//
+//  Created by vatran robert on 2023-08-07.
+//
+
+import SwiftUI
+
+@main
+struct BrickorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
